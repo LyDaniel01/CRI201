82e49a483b33224226d112fe98a3888496024f26
lyDaniel01 <dlaymerodas@gmail.com>
2026-08-24 07:47:29 -0400

    Se crea el directorio de tareas + tarea1

--- Ejercicios/Cifrado_simetrico.py
+++ Ejercicios/Cifrado_simetrico.py
@@ -3,22 +3,24 @@ class Cifrado_Simetrico:
     def __init__(self, mensaje, clave):
         self.mensaje = mensaje
         self.clave = clave
+
     def getMensaje(self):
         return self.mensaje
+        
     def getClave(self):
         return self.clave
 
     def cifrar(self):
         cadenaCifrada = ""
         for i in range(len(self.getMensaje())):
-            cadenaCifrada += format((ord(self.getMensaje()[i])^ ord (clave[i%len(clave)])),"02x")
+            cadenaCifrada += format((ord(self.getMensaje()[i]) ^ ord (clave[i%len(clave)])),"02x")
         print(cadenaCifrada)
         return cadenaCifrada
           
     def descifrar(self, mensajeCifrado):
         mensajeDescifrado = ""
         for i in range(0,len(mensajeCifrado),2):
-            mensajeDescifrado += chr(int(mensajeCifrado[i:i+2],16)^ ord(self.getClave()[i//2 % len(self.getClave())]))
+            mensajeDescifrado += chr(int(mensajeCifrado[i:i+2],16) ^ ord(self.getClave()[i//2 % len(self.getClave())]))
         print(mensajeDescifrado)
 
 os.system('cls')
@@ -26,4 +28,4 @@ mensaje = input("Introduzca el mensaje: ")
 clave = input("Introduzca la clave: ")
 objeto = Cifrado_Simetrico(mensaje, clave)
 resultadoCifrado = objeto.cifrar()
-objeto.descifrar(resultadoCifrado)
+objeto.descifrar(resultadoCifrado) 
